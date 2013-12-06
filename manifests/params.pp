8a198111c57d4858cb736f4ece967c0a21d1d3d7
Samuli Seppänen <samuli.seppanen@gmail.com>
2013-12-06 20:25:17 +0200

    Made monit start/stop rules OS-agnostic
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,22 +10,31 @@ class snmpd::params {
             $package_name = ['net-snmp-utils','net-snmp']
             $config_name = '/etc/snmpd/snmpd.conf'
             $service_name = 'snmpd'
-            $service_command = "/sbin/service $service_name"            
             $pidfile = '/var/run/snmpd.pid'
+
+            if $::operatingsystem == 'Fedora' {
+                $service_start = "/usr/bin/systemctl start ${service_name}.service"
+                $service_stop = "/usr/bin/systemctl stop ${service_name}.service"
+            } else {
+                $service_start = "/sbin/service $service_name start"
+                $service_stop = "/sbin/service $service_name stop"
+            }
         }
         'Debian': {
             $package_name = 'snmpd'
             $config_name = '/etc/snmp/snmpd.conf'
             $service_name = 'snmpd'
-            $service_command = "/usr/sbin/service $service_name" 
             $pidfile = '/var/run/snmpd.pid'
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
         'FreeBSD': {
             $package_name = 'net-snmp'            
             $config_name = '/usr/local/etc/snmpd.conf'
             $service_name = 'snmpd'
-            $service_command = "/usr/local/etc/rc.d/$service_name" 
             $pidfile = '/var/run/net_snmpd.pid'
+            $service_start = "/usr/local/etc/rc.d/$service_name start"
+            $service_stop = "/usr/local/etc/rc.d/$service_name stop"
         }
         default: {
             $package_name = 'snmpd'
@@ -33,6 +42,8 @@ class snmpd::params {
             $service_name = 'snmpd'
             $service_command = "/usr/sbin/service $service_name" 
             $pidfile = '/var/run/snmpd.pid'
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
     }
 }
