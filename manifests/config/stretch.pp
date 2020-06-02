0ca8ac9fe837731771d1e963ac0db6ca1f330da6
Samuli Seppänen <samuli@openvpn.net>
2020-06-02 16:26:10 +0300

    Fix system unit file override on Debian 9
    Make it use the camptocamp approach
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/config/stretch.pp
+++ manifests/config/stretch.pp
@@ -5,28 +5,11 @@
 # Debian 9 ("stretch").
 #
 class snmpd::config::stretch inherits snmpd::params {
-
-    # Most of this copy-and-past from the systemd module's service_fragment 
-    # define
-    $service_name = $::snmpd::params::service_name
-    $fragment_dir = "/etc/systemd/system/${service_name}.service.d"
-
-    File {
-        owner  => $::os::params::adminuser,
-        group  => $::os::params::admingroup,
-        mode   => '0755',
-    }
-
-    file { "systemd-${service_name}.service.d":
-        ensure => 'directory',
-        name   => $fragment_dir,
-    }
-
-    file { "systemd-${service_name}-puppet.conf":
-        ensure  => 'present',
-        name    => "${fragment_dir}/puppet.conf",
-        content => template('snmpd/puppet.conf.erb'),
-        require => File["systemd-${service_name}.service.d"],
-        notify  => [ Class['systemd::service'], Class['snmpd::service'] ],
+    ::systemd::dropin_file { 'snmpd':
+        ensure   => 'present',
+        unit     => "${::snmpd::params::service_name}.service",
+        content  => template('snmpd/puppet.conf.erb'),
+        filename => 'puppet.conf',
+        mode     => '0755',
     }
 }
